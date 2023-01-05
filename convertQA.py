30930e5baef68285a5917ee616bee486f230e0b2
Aleksey <rusal@bk.ru>
2023-01-06 00:41:35 +0300

    add convert

---
+++ convertQA.py
@@ -0,0 +1,7 @@
+﻿import json
+
+if __name__ == "__main__":
+    filename = "d:/ml/chat/ChatDEEP.json"
+    with open("quotes_keywords.json", "r", encoding="UTF8") as file:
+        content = file.read()
+    quotes_keywords = json.loads(content)
